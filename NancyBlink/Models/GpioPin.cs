843a7cefa2b6219d2b6d8ed60d4b0a41297129b1
Nate Palmer <np@configit.com>
2014-05-29 08:23:52 -0400

    Added model, added post method

---
+++ NancyBlink/Models/GpioPin.cs
@@ -0,0 +1,47 @@
+﻿using Raspberry.IO.GeneralPurpose;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyBlink.Models
+{
+    public class GpioPin
+    {
+        public bool Powered { get; set; }
+        public int PinNumber { get; set; }
+    }
+
+    public static class PinExtensions
+    {
+        public static ConnectorPin ToConnectorPin (this GpioPin pin)
+        {
+            switch (pin.PinNumber)
+            {
+                case 3:
+                    return ConnectorPin.P1Pin03;
+                case 5:
+                    return ConnectorPin.P1Pin05;
+                case 7:
+                    return ConnectorPin.P1Pin07;
+                case 8: 
+                    return ConnectorPin.P1Pin08;
+                case 10:
+                    return ConnectorPin.P1Pin10;
+                case 11: 
+                    return ConnectorPin.P1Pin11;
+                case 12:
+                    return ConnectorPin.P1Pin12;
+                case 13:
+                    return ConnectorPin.P1Pin13;
+                case 15:
+                    return ConnectorPin.P1Pin15;
+                case 16:
+                    return ConnectorPin.P1Pin16;
+                default:
+                    throw new InvalidOperationException("Pin not implemented");
+            }
+        }
+    }
+}
--- NancyBlink/Program.cs
+++ NancyBlink/Program.cs
@@ -1,30 +1,39 @@
-using Nancy;
 using System;
+
+using Nancy;
+using Nancy.ModelBinding;
 using Raspberry.IO.GeneralPurpose;
+using NancyBlink.Models;
 
 
 namespace Example
 {
-        public class Program:Nancy.NancyModule
+    public class Program : NancyModule
+    {
+        static void Main(string[] args)
         {
-                static void Main(string[] args) {
-                        Console.Write("Starting server...");
-                        var server = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8282"));
-                        server.Start();
-                        Console.WriteLine("started on port 8282!");
-                        Console.WriteLine("press any key to exit");
-                        Console.Read();
-                }
-
-                public Program()
-                {
-                        Get["/"] = _ => { 
-                            OutputPinConfiguration red = ConnectorPin.P1Pin03.Output();
-                            var connection = new GpioConnection(red);
-                            connection[red] = false;
-
-                            return "Nancy says hello!"; 
-                        };
-                }
+            Console.Write("Starting server...");
+            var server = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8282"));
+            server.Start();
+            Console.WriteLine("started on port 8282!");
+            Console.WriteLine("press any key to exit");
+            Console.Read();
         }
+
+        public Program()
+        {
+            Get["/"] = _ =>
+            {
+                return "Use this api to turn on various lights connected to the Pi's GPIO Pins";
+            };
+            Post["/GpioPin"] = _ =>
+            {
+                var gpioPin = this.Bind<GpioPin>();
+                OutputPinConfiguration pin = gpioPin.ToConnectorPin().Output();
+                //var connection = new GpioConnection(pin);
+                //connection[pin] = gpioPin.Powered;
+                return gpioPin;
+            };
+        }
+    }
 }
